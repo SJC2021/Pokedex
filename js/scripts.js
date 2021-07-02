9087d8e8bd411a5a0e01e1b926363e6dda21c0e1
SJC2021 <82860116+SJC2021@users.noreply.github.com>
2021-07-02 12:30:34 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,57 @@
-let pokemonList = [{name: "Gengar", height: 1.5, types: ["fighting" ,"ghost"]},
-                   {name: "Hypno", height: 1.6, type: ["psychic"]},
-                   {name: "Breloom", height: 1.2, type: ["flying", "grass"]}
-                 ];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Gengar",
+      type: ["fighting", "ghost"],
+      height: 1.5
+    },
+    {
+      name: "Hypno",
+      type: ["psychic"],
+      height: 1.6
+    },
+    {
+      name: "Breloom",
+      type: ["flying", "grass"],
+      height: 1.2
+    },
+  ];
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
 
-for (let i=0; i < pokemonList.length; i++) {
-                     //Writes Pokemon name to the DOM
-                     document.write(pokemonList[i].name + " (height :" + pokemonList[i].height + ")");
-                   //Checks if pokemon height is greater than 1.5 m
-                     if (pokemonList[i].height > 1.5) {
-                       //if pokemon height is greater than 1.6 m, writes "Wow that's big!"
-                       document.write(" - Wow, that's big!");
-                     }
-                     //Adds line breaks after each pokemon name
-                     document.write("<br><br>");
-                   }
+    function getAll() {
+      return pokemonList;
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add(
+  {
+  name: 'Turtwig',
+  type: ["ice"],
+  height: 0.4
+});
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + " " + "height : " + pokemon.height + " " + pokemon.type + "</br>");
+});
+
+/* for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + "- Height: " + pokemonList[i].height)
+  console.log(pokemonList[i].name + " Height: " + pokemonList[i].height)
+if ([pokemonList[i].height] < 0.5) {
+  document.write(" - This Pokemon is SMOL!")
+}
+ document.write("<br>");
+} */
+
+/*pokemonList.forEach(function(pokemon){
+  console.log(pokemon);
+});*/
